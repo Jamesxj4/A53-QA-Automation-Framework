34af76970d379673c512fecaa67389f70d2ff9df
Jamesxj4 <james.lu@testpro.io>
2024-05-14 10:40:21 -0400

    scroll down findElement
    JavascriptExecutor scroll down findElement

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -3,9 +3,15 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class
 LoginTests extends BaseTest {
+
+
 
     @Test
     public void navigateToKoelApp() {
@@ -16,11 +22,12 @@ LoginTests extends BaseTest {
     public void loginValidEmailPassword() throws InterruptedException {
         //try {
             //Thread.sleep(2000);
-            provideEmail("demo@class.com");
+            provideEmail("james.lu@testpro.io");
             //Thread.sleep(2000);
-            providePassword("te$t$tudent");
+            providePassword("QnNBjg75$$");
             //Thread.sleep(2000);
             clickSubmit();
+        Thread.sleep(2000);
             //Assertion
             //WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
             WebElement avatarIcon = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img[class='avatar']")));
@@ -28,6 +35,19 @@ LoginTests extends BaseTest {
        // }catch (Exception e){
        //    System.out.println("Something went wrong !" +e);
        // }
+
+//        JavascriptExecutor js = (JavascriptExecutor)driver;
+//        driver.manage().timeouts().setScriptTimeout(1,TimeUnit.SECONDS);
+//        js.executeAsyncScript("window.scrollBy(0,document.body.scrollHeight)");
+//        Thread.sleep(4000);
+//        WebElement topAlbumsIcon = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"homeWrapper\"]/div/section[3]/h1")));
+//        WebElement topAlbumsIcon = driver.findElement(By.xpath("//*[@id=\"homeWrapper\"]/div/section[3]/h1"));
+//        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", topAlbumsIcon);
+//        Thread.sleep(5000);
+//
+//        Assert.assertTrue(topAlbumsIcon.isDisplayed());
+
+        //*[@id="homeWrapper"]/div/section[3]/h1
 
     }
 
